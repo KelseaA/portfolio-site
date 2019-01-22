2393b4879d118dbbb321629e5eb393b0848cb451
Kelsea Anderson <andersonkelsea@gmail.com>
2019-01-21 20:18:49 -0600

    add arrow functionality to slider

--- site/js/app.js
+++ site/js/app.js
@@ -1,21 +1,36 @@
 var thumbnail = document.getElementsByClassName("js-thumbnail");
+var arrow = document.getElementsByClassName("js-arrow");
+var slideIndex = 1;
 
-function addEventListener() {    
+function addEventListeners() {    
     for (var i = 0; i < thumbnail.length; i++) {
         thumbnail[i].addEventListener("click", function() {
             var thumbnailIndex = this.getAttribute("data-index");
             changeMainImage(slideIndex = thumbnailIndex);
         });
     }
+
+    for (var i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener("click", function(e) {
+            e.preventDefault();
+
+            var increment = parseInt(this.getAttribute("data-increment"));
+            changeMainImage(slideIndex += increment);
+        });
+    }
 }    
 
-function changeMainImage(thumbnailIndex) {
+function arrowMainImage(n) {
+    changeMainImage(slideIndex += n);
+}
+
+function changeMainImage(n) {
     var slide = document.getElementsByClassName("js-slide");
 
-    if (thumbnailIndex > slide.length) {
+    if (n > slide.length) {
         slideIndex = 1;
     }
-    if (thumbnailIndex < 1) {
+    if (n < 1) {
         slideIndex = slide.length;
     }
     for (var i = 0; i < slide.length; i++) {
@@ -30,6 +45,6 @@ function changeMainImage(thumbnailIndex) {
 
 (function() {
     if (thumbnail.length) {
-        addEventListener();
+        addEventListeners();
     }          
 })();
