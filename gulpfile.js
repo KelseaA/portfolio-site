10622b6accc048d8f154b2fac096228c421ec832
Kelsea Anderson <andersonkelsea@gmail.com>
2019-02-11 22:10:27 -0600

    minor update

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('default', function (callback) {
 
 gulp.task('build', function (callback) {
   runSequence('clean:dist', 
-    ['sass', 'useref', 'images', 'fonts'],
+    ['sass'],
     callback
   )
 })
